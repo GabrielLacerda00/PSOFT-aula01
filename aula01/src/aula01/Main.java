87655f12e27b83a9fbc9e39ca84c2aa5407c85e8
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2022-05-09 10:03:26 -0300

    aula01-psoft

--- aula01/src/aula01/Main.java
+++ aula01/src/aula01/Main.java
@@ -1,9 +1,11 @@
 package aula01;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Produto produto = new Produto(9,"leite","FazendaMinerva");
+		Lote lote = new Lote(10,produto,20);
 
 	}
 
